d4a2b6a21a0c545d814f1c47c71e34131f6b601e
MOHIT KUMAR <mohitkumar221000@gmail.com>
2021-07-25 14:16:22 +0530

    upadted input values

--- merge_two_arrays.c
+++ merge_two_arrays.c
@@ -44,7 +44,7 @@ struct Array* Merge(struct Array *arr1, struct Array *arr2)
 int main()
 {
     struct Array arr1={{2,6,10,15,25},10,5};
-    struct Array arr2={{3,4,7,18,20},10,5};
+    struct Array arr2={{3,4,7,18,22},10,5};
     struct Array *arr3;
 
     arr3=Merge(&arr1,&arr2);
